7509a68cc3f5ed1d132a166c00b617e405d79a36
Zachary Frederick <zachfred62@gmail.com>
2019-05-22 18:44:02 -0400

    getting ready to deploy to heroku
    :( why jash

--- nyte_backend/settings.py
+++ nyte_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 AUTH_USER_MODEL = 'api.NyteUser'
 
@@ -134,3 +137,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication', #debug purposes only
     ],
 }
+
+django_heroku.settings(locals())
