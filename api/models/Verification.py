8dda754fa824b45ab896cafb2de4caf218332898
Zachary Frederick <zachfred62@gmail.com>
2019-06-07 19:38:57 -0400

    small tweaks

--- api/models/Verification.py
+++ api/models/Verification.py
@@ -11,6 +11,7 @@ class Verification(models.Model):
     user = models.OneToOneField(NyteUser, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100, default="")
     last_name = models.CharField(max_length=100, default="")
+    email = models.CharField(max_length=100, default="")
     dob_day = models.CharField(max_length=100, default="")
     dob_month = models.CharField(max_length=100, default="")
     dob_year = models.CharField(max_length=100, default="")
@@ -19,7 +20,7 @@ class Verification(models.Model):
     state = models.CharField(max_length=50, default="")
     zipcode = models.CharField(max_length=10, default="")
     uuid = models.CharField(max_length=200, default="")
-    status = models.CharField(max_length=100, default="")
+    verif_status = models.CharField(max_length=100, default="")
     attempted_to_verify = models.BooleanField(default=False)
     error_code = models.CharField(max_length=100, default="")
     error_msg = models.CharField(max_length=100, default="")
@@ -32,9 +33,9 @@ class Verification(models.Model):
         if response is not None:
             if response.error_code is None:
                 self.uuid = response.uuid
-                self.status = response.status
+                self.verif_status = response.status
 
-                if self.status == "accepted":
+                if self.verif_status == "accepted":
                     self.user.update_verification_info(self)
             else:
                 self.error_code = response.error_code
