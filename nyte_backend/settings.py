452242bfc8346e1d411d5368797b7d76996cf9c7
Zachary Frederick <zachfred62@gmail.com>
2019-07-07 22:06:24 -0400

    idk

--- nyte_backend/settings.py
+++ nyte_backend/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(default='postgres://zach:HelloEd12@localhost:5433/nyte', conn_max_age=600)
 
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
