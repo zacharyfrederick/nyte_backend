8b9c32e919c97caa916290e51f728b2eef4d59ed
Zachary Frederick <zachfred62@gmail.com>
2019-07-11 13:52:39 -0400

    added sales tax and convenience fee to venue

--- api/models/Venue.py
+++ api/models/Venue.py
@@ -9,6 +9,8 @@ class Venue(models.Model):
     tagline = models.CharField(max_length=100, blank=False, null=True)
     image = models.CharField(max_length=100) #TODO: install pillow and change to image field
     markup_fee = models.FloatField(null=True, blank=True)
+    sales_tax = models.FloatField(null=True, blank=True, default=6.0); #represents a 6% sales sax
+    convenience_fee = models.FloatField(null=True, blank=True, default=0.5);
     
     def __str__(self):
         return self.name;
