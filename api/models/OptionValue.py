3a89154ab8a0d05e8c71ed890413cb4a4e724f62
Zachary Frederick <zachfred62@gmail.com>
2019-08-10 17:06:40 -0400

    Update OptionValue.py

--- api/models/OptionValue.py
+++ api/models/OptionValue.py
@@ -3,6 +3,6 @@ from .Option import MenuOption
 
 class OptionValue(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
-    price = models.IntegerField(default=0, null=True, blank=True)
+    price = models.FloatField(default=0.0, null=True, blank=True)
     default = models.BooleanField(default=False, blank=True, null=True)
     option = models.ForeignKey(MenuOption, on_delete=models.CASCADE, default=None, null=True, blank=True,related_name='values')    
