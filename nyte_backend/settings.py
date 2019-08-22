8203bbac82f9f3560addab6aa1d75bd160a75f10
Zachary Frederick <zachfred62@gmail.com>
2019-08-22 19:12:53 -0400

    Update settings.py

--- nyte_backend/settings.py
+++ nyte_backend/settings.py
@@ -173,7 +173,10 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 #facebook auth settings
