51f9289c515804d4bafeb20030d66d6333d80566
Zachary Frederick <zachfred62@gmail.com>
2019-08-26 18:30:14 -0400

    Update createsu.py

--- api/management/commands/createsu.py
+++ api/management/commands/createsu.py
@@ -8,3 +8,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not NyteUser.objects.filter(email="zachfred62@gmail.com").exists():
             NyteUser.objects.create_superuser(email="zachfred62@gmail.com", password="HelloEd12")
+
+        if not NyteUser.objects.filter(email="josh@langinc.com").exists():
+            NyteUser.objects.create_superuser(email="josh@langinc.com", password="Hughsucksfarts")
+        
+        if not NyteUser.objects.filter(email="kyle@getnyte.com").exists():
+            NyteUser.objects.create_superuser(email="kyle@getnyte.com", password="Nyte123!")
