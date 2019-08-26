bd1655ef7422b96692719af54ac5a2c04968c990
Zachary Frederick <zachfred62@gmail.com>
2019-08-26 18:23:50 -0400

    who knows

--- api/management/commands/createsu.py
+++ api/management/commands/createsu.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from ...models import NyteUser
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("zachary", "zachfred62@gmail.com", "HelloEd12")
+        if not NyteUser.objects.filter(email="zachfred62@gmail.com").exists():
+            NyteUser.objects.create_superuser(email="zachfred62@gmail.com", password="HelloEd12")
